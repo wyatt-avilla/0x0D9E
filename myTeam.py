df535754931af084b314633334a0eabd7785bfe7
wyatt-avilla <wyattmurphy1@gmail.com>
2024-11-21 16:19:15 -0800

    dummy agent from batuhan announcement

---
+++ myTeam.py
@@ -0,0 +1,79 @@
+import random
+
+from pacai.agents.capture.capture import CaptureAgent
+
+def createTeam(firstIndex, secondIndex, isRed):
+    """
+    This function should return a list of two agents that will form the capture team,
+    initialized using firstIndex and secondIndex as their agent indexed.
+    isRed is True if the red team is being created,
+    and will be False if the blue team is being created.
+    """
+
+    firstAgent = DummyAgent1
+    secondAgent = DummyAgent2
+
+    return [
+        firstAgent(firstIndex),
+        secondAgent(secondIndex),
+    ]
+
+# Copied over from pacai/agents/capture/dummy.py 
+class DummyAgent1(CaptureAgent):
+    """
+    A Dummy agent to serve as an example of the necessary agent structure.
+    You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the agent and populates useful fields,
+        such as the team the agent is on and the `pacai.core.distanceCalculator.Distancer`.
+
+        IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
+        """
+
+        super().registerInitialState(gameState)
+
+        # Your initialization code goes here, if you need any.
+
+    def chooseAction(self, gameState):
+        """
+        Randomly pick an action.
+        """
+
+        actions = gameState.getLegalActions(self.index)
+        return random.choice(actions)
+
+# Copied over from pacai/agents/capture/dummy.py 
+class DummyAgent2(CaptureAgent):
+    """
+    A Dummy agent to serve as an example of the necessary agent structure.
+    You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the agent and populates useful fields,
+        such as the team the agent is on and the `pacai.core.distanceCalculator.Distancer`.
+
+        IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
+        """
+
+        super().registerInitialState(gameState)
+
+        # Your initialization code goes here, if you need any.
+
+    def chooseAction(self, gameState):
+        """
+        Randomly pick an action.
+        """
+
+        actions = gameState.getLegalActions(self.index)
+        return random.choice(actions)
