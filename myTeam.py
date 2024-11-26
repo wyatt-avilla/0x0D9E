b22a3626107043039af25f531dec6c19a73cbaa1
wyatt-avilla <wyattmurphy1@gmail.com>
2024-11-25 16:50:59 -0800

    cleaned example code and added types

--- myTeam.py
+++ myTeam.py
@@ -1,8 +1,23 @@
 import random
+from enum import Enum
+from typing import Any
 
 from pacai.agents.capture.capture import CaptureAgent
+from pacai.bin.capture import CaptureGameState
+from pacai.core.directions import Directions
 
-def createTeam(firstIndex, secondIndex, isRed):
+
+class Action(Enum):
+    NORTH = Directions.NORTH
+    SOUTH = Directions.SOUTH
+    EAST = Directions.EAST
+    WEST = Directions.WEST
+    STOP = Directions.STOP
+
+
+
+
+def createTeam(firstIndex: int, secondIndex: int, isRed: bool) -> list[CaptureAgent]:
     """
     This function should return a list of two agents that will form the capture team,
     initialized using firstIndex and secondIndex as their agent indexed.
@@ -10,25 +25,26 @@ def createTeam(firstIndex, secondIndex, isRed):
     and will be False if the blue team is being created.
     """
 
-    firstAgent = DummyAgent1
-    secondAgent = DummyAgent2
+    firstAgent = DummyAgent
+    secondAgent = DummyAgent
 
     return [
         firstAgent(firstIndex),
         secondAgent(secondIndex),
     ]
 
-# Copied over from pacai/agents/capture/dummy.py 
-class DummyAgent1(CaptureAgent):
+
+# Copied over from pacai/agents/capture/dummy.py
+class DummyAgent(CaptureAgent):
     """
     A Dummy agent to serve as an example of the necessary agent structure.
     You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
     """
 
-    def __init__(self, index, **kwargs):
+    def __init__(self, index: int, **kwargs: dict[str, Any]) -> None:
         super().__init__(index, **kwargs)
 
-    def registerInitialState(self, gameState):
+    def registerInitialState(self, gameState) -> None:
         """
         This method handles the initial setup of the agent and populates useful fields,
         such as the team the agent is on and the `pacai.core.distanceCalculator.Distancer`.
@@ -40,37 +56,7 @@ class DummyAgent1(CaptureAgent):
 
         # Your initialization code goes here, if you need any.
 
-    def chooseAction(self, gameState):
-        """
-        Randomly pick an action.
-        """
-
-        actions = gameState.getLegalActions(self.index)
-        return random.choice(actions)
-
-# Copied over from pacai/agents/capture/dummy.py 
-class DummyAgent2(CaptureAgent):
-    """
-    A Dummy agent to serve as an example of the necessary agent structure.
-    You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
-    """
-
-    def __init__(self, index, **kwargs):
-        super().__init__(index, **kwargs)
-
-    def registerInitialState(self, gameState):
-        """
-        This method handles the initial setup of the agent and populates useful fields,
-        such as the team the agent is on and the `pacai.core.distanceCalculator.Distancer`.
-
-        IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
-        """
-
-        super().registerInitialState(gameState)
-
-        # Your initialization code goes here, if you need any.
-
-    def chooseAction(self, gameState):
+    def chooseAction(self, gameState: CaptureGameState) -> list[Action]:
         """
         Randomly pick an action.
         """
